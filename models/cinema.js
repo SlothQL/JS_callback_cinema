c8e76f51fd709700eb180b7f8b8712959885888d
Alina <lindenalina.al@gmail.com>
2020-07-22 14:19:24 +0100

    check if film from year exists passed

--- models/cinema.js
+++ models/cinema.js
@@ -14,4 +14,8 @@ Cinema.prototype.listByGenre = function(genre) {
   return this.films.filter(film => film.genre === genre);
 }
 
+Cinema.prototype.filmFromYear = function(year) {
+  return this.films.some(film => film.year === year);
+}
+
 module.exports = Cinema;
